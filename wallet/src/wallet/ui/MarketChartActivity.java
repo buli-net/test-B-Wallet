b5008f7142c4d859a1336869cdbabd8bab2cc193
buli <hvlrcb@gmail.com>
2026-08-23 12:26:44 +0700

    Add MarketChartActivity for market chart UI

---
+++ wallet/src/wallet/ui/MarketChartActivity.java
@@ -0,0 +1,13 @@
+package wallet.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import wallet.R;
+
+public class MarketChartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_chart);
+    }
+}
