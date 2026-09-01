334a1b1a4fd9f4235d7b220862f10dca47dcde42
buli <hvlrcb@gmail.com>
2026-09-01 12:30:06 +0700

    Update MarketChartActivity.java

--- wallet/src/wallet/ui/MarketChartActivity.java
+++ wallet/src/wallet/ui/MarketChartActivity.java
@@ -958,7 +958,7 @@ public class MarketChartActivity extends Activity implements ViewModelStoreOwner
 
         if (btnAddMa!= null) {
             btnAddMa.setOnClickListener(v -> {
-                if (state.tempList.size() >= 28) { // limit add MA
+                if (state.tempList.size() >= 10) { // limit add MA
                     Toast.makeText(v.getContext(),
                             getString(R.string.max_ma_reached),
                             Toast.LENGTH_SHORT).show();
